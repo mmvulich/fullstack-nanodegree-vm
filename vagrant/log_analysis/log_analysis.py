b2dd24ae6db670a70982434ec456240623eee903
Matt Vulich <mmvulich@gmail.com>
2018-11-15 09:12:48 -0500

    Select Query for first question

--- vagrant/log_analysis/log_analysis.py
+++ vagrant/log_analysis/log_analysis.py
@@ -5,3 +5,16 @@
 #Creation Date: 2018-11-15
 #
 #############################
+
+import psycopg2
+
+#set database name
+DBNAME = 'dbname=news'
+
+#Find top 3 articles of all time
+db = psycopg2.connect(DBNAME)
+c = db.cursor()
+c.execute('select a.title, count(l.id) as views from articles as a, log as l where (l.path like concat("%", a.slug) and l.status = "200 OK") group by a.title order by views desc limit 3')
+articles = c.fetchall()
+print(articles)
+db.close()
