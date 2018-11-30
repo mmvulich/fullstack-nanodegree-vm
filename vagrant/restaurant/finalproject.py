8b4d22ad5015e3e5ed4db77295a2e5e494170a83
Matt Vulich <mmvulich@gmail.com>
2018-11-30 11:54:10 -0500

    Added templates and Forms

--- vagrant/restaurant/finalproject.py
+++ vagrant/restaurant/finalproject.py
@@ -1,39 +1,53 @@
-from flask import Flask
+from flask import Flask, render_template
 app= Flask(__name__)
+
+#Fake Restaurants
+restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
+
+restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
+
+len_restaurants = len(restaurants)
+
+#Fake Menu Items
+items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
+
+len_items = len(items)
+
+item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
 
 @app.route('/')
 @app.route('/restaurants/')
 def showRestaurants():
-    return "This page will show all my restaurants"
+    return render_template('restaurants.html', restaurants = restaurants, len_restaurants = len_restaurants)
 
 @app.route('/restaurants/new')
 def newRestaurant():
-    return "This page will be for making a new restaurant"
+    return render_template('newRestaurant.html', restaurants = restaurants)
 
 @app.route('/restaurants/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-    return "this page will be for editing %s" % restaurant_id
+    return render_template('editRestaurant.html', restaurant = restaurant, restaurant_id = restaurant_id)
 
 @app.route('/restaurants/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-    return "This page will be for deleting restaurant %s" % restaurant_id
+    return render_template('deleteRestaurant.html', restaurant = restaurant, restaurant_id = restaurant_id)
 
 @app.route('/restaurants/<int:restaurant_id>/')
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-    return "This page is the menu for restaurant %s" % restaurant_id
+    return render_template('menu.html', restaurant_id = restaurant_id, items = items, restaurant = restaurant, len_items = len_items)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-    return "This page is for making a new menu item for restaurant %s" % restaurant_id
+    return render_template('newMenuItem.html', restaurant_id = restaurant_id, items = items, restaurant = restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-    return "this page is for editing menu item %s" % menu_id
+    return render_template('editMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item, restaurant = restaurant)
 
 @app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-    return "This page is for deleting menu item %s" % menu_id
+    return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item, items = items)
 
 if __name__ == '__main__':
     app.debug = True
