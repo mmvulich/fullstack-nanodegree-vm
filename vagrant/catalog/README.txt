6ce9cf8b914fc4bc5ea565627ad0036662e3d81d
Matt Vulich <mmvulich@gmail.com>
2019-01-04 12:18:48 -0500

    edited README.txt

--- vagrant/catalog/README.txt
+++ vagrant/catalog/README.txt
@@ -1,1 +1,45 @@
-Place your catalog project in this directory.
+# Catalog Application Project
+
+This Catalog Application allows a user to view different sports and view the equipment associated with that sport. If a user is logged in they can add a new item and edit/delete items they own.
+
+## Prerequisites
+
+### Installation
+In order to run the script make sure to install the following packages:
+
+* `flask`
+* `oauth2client`
+* `httplib2`
+* `json`
+* `requests`
+* `flask_httpauth`
+* `sqlalchemy`
+
+You can install these packages by running the following command for each package:
+
+`$ pip install <package>`
+or
+`$ pip3 install <package>`
+
+depending on what version of pip you have installed
+
+### Database
+In order for this application to run the catalog.db database must be in the same folder as the application.py file. This database can be found [here](https://github.com/mmvulich/fullstack-nanodegree-vm/blob/master/vagrant/catalog/catalog.db)
+
+### Authorization
+In order to use the application for more than viewing the database (i.e. new/edit/delete) you will need a google account. If you do not have one you can sign up for one for free [here](https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dsign%2Bup%2Bfor%2Bgoogle%2Baccount%26rlz%3D1C1CHBF_enUS817US817%26oq%3Dsign%2Bup%2Bfor%2Bgoogle%2Baccount%26aqs%3Dchrome..69i57j0l5.3425j1j7%26sourceid%3Dchrome%26ie%3DUTF-8&flowName=GlifWebSignIn&flowEntry=AddSession)
+
+## Usage
+
+### Kicking off Application
+To run the Catalog Application script `cd` to the directory where the script is saved and then run the following command:
+
+`$ python application.py'
+
+### Interacting in the Browser
+Open your favorite browser and navagate to [http://localhost:8000/](http://localhost:8000/)
+
+This will bring you to the main page of application where you can see a list of all the sports in the catalog and the last 6 equipment items that have been added. You can click on a sport to view all the equipment associated with that sport or click on an equipment item to get a detailed description.
+
+Logging into the application using your google login credentials will allow you to add an item to a sport and edit/delete any items that you own.
+
--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
-app= Flask(__name__)
-
-from sqlalchemy import create_engine, desc, asc
-from sqlalchemy.orm import sessionmaker
+#!/usr/bin/env python3
+#
+# Author: Matt Vulich
+#
+# Creation Date: 2019-01-04
+#
+##############################################################################
+from flask import Flask, render_template, request, redirect
+from flask import url_for, jsonify, flash
 from catalog_database_setup import Base, Category, CategoryItem, User
-
 from flask import session as login_session
 import random
 import string
@@ -15,10 +18,15 @@ import json
 from flask import make_response
 import requests
 from flask_httpauth import HTTPBasicAuth
+from sqlalchemy import create_engine, desc, asc
+from sqlalchemy.orm import sessionmaker
 
+app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(open('client_secrets.json', 'r')
+                       .read())['web']['client_id']
+
 APPLICATION_NAME = "Catalog-Project"
 
 engine = create_engine('sqlite:///catalog.db?check_same_thread=False')
@@ -27,41 +35,48 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#Directs to main catalog page
+# Directs to main catalog page
+
+
 @app.route('/')
 @app.route('/catalogs/')
 def catalogs():
     access_token = login_session.get('access_token')
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
-    print url
     categories = session.query(Category)
     categories_count = session.query(Category).count()
-    items = session.query(CategoryItem).order_by(desc(CategoryItem.id)).limit(categories_count)
+    items = session.query(CategoryItem).order_by(desc(CategoryItem.id))\
+        .limit(categories_count)
     if 'username' not in login_session:
-        return render_template('publicCatalog.html', categories = categories, items = items)
+        return render_template('publicCatalog.html',
+                               categories=categories, items=items)
     else:
-        return render_template('catalog.html', categories = categories, items = items)
+        return render_template('catalog.html',
+                               categories=categories, items=items)
 
-#Directs to login page using OAuth2.0
+# Directs to login page using OAuth2.0
+
+
 @app.route('/login/')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) 
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    return render_template('login.html', STATE = state)
+    return render_template('login.html', STATE=state)
 
-@app.route('/gconnect', methods = ['POST'])
+
+@app.route('/gconnect', methods=['POST'])
 def gconnect():
-    # Validate state token
+    # Validating the state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    # Obtain authorization code
+    # Obtaining the authorization code
     code = request.data
 
     try:
-        # Upgrade the authorization code into a credentials object
+        # Need to upgrade the authorization code into a credentials object
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
@@ -71,19 +86,19 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Check that the access token is valid.
+    # Validating the access token
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
+    # Abort if any errors in the access token
     if result.get('error') is not None:
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is used for the intended user.
+    # Verifying if the access token is for the intended user
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
         response = make_response(
@@ -91,7 +106,7 @@ def gconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Verify that the access token is valid for this app.
+    # Verifying that the access token is valid for this app
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
@@ -102,16 +117,17 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
+        response = make_response(json
+                                 .dumps('Current user is already connected.'),
                                  200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
-    # Store the access token in the session for later use.
+    # Storing the accesses token for future use
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    # Get user info
+    # Grabing user info
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
@@ -122,7 +138,7 @@ def gconnect():
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
 
-    # see if user exists, if it doesn't make a new one
+    # If user does not exist create a new one
     user_id = getUserID(data["email"])
     if not user_id:
         user_id = createUser(login_session)
@@ -134,13 +150,14 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;\
+        border-radius: 150px;-webkit-border-radius:\
+        150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
 
 # User Helper Functions
-
 
 
 def createUser(login_session):
@@ -152,21 +169,20 @@ def createUser(login_session):
     return user.id
 
 
-
 def getUserInfo(user_id):
     user = session.query(User).filter_by(id=user_id).one()
     return user
-
 
 
 def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
-    except:
+    except IOerror:
         return None
 
 # DISCONNECT - Revoke a current user's token and reset their login_session
+
 
 @app.route('/gdisconnect')
 def gdisconnect():
@@ -201,95 +217,122 @@ def gdisconnect():
         return response
 
 
-#Directs to specific category page
-
+# Directs to specific category page
 
 
 @app.route('/catalogs/<string:category_name>/items/')
 def catalogCategory(category_name):
     categories = session.query(Category)
-    category = session.query(Category).filter_by(name = category_name).one()
-    items = session.query(CategoryItem).filter_by(category_id = category.id)
+    category = session.query(Category).filter_by(name=category_name).one()
+    items = session.query(CategoryItem).filter_by(category_id=category.id)
     num_items = items.count()
     if num_items == 0 or num_items > 1:
         item_string = 'items'
     else:
         item_string = 'item'
-    return render_template('items.html', category = category, items = items, num_items = num_items, item_string = item_string, categories = categories)
+    return render_template('items.html', category=category, items=items,
+                           num_items=num_items, item_string=item_string,
+                           categories=categories)
 
-#Directs to specific item description page
+# Directs to specific item description page
+
 
 @app.route('/catalogs/<string:category_name>/items/<string:item_name>/')
 def catalogItem(category_name, item_name):
-    category = session.query(Category).filter_by(name = category_name).one()
-    item = session.query(CategoryItem).filter_by(name = item_name, category_id = category.id).first()
+    category = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(CategoryItem)\
+        .filter_by(name=item_name, category_id=category.id).first()
+    # If user is not logged in they are directed to the public page
     if 'username' not in login_session:
-        return render_template('publicItemDescription.html', item = item)
-    elif item != None:
-        return render_template('item_description.html', item = item)
+        return render_template('publicItemDescription.html', item=item)
+    elif item is not None:
+        return render_template('item_description.html', item=item)
     else:
         return "Item Does not Exist!"
 
-#Directs to add a new item page
+# Directs to add a new item page
 
-@app.route('/catalogs/new/', methods = ['GET', 'POST'])
+
+@app.route('/catalogs/new/', methods=['GET', 'POST'])
 def newItem():
+    # Directs to login page if not logged in
     if 'username' not in login_session:
         return redirect('/login/')
     categories = session.query(Category).all()
     items = session.query(CategoryItem).all()
     if request.method == 'POST':
-        category = session.query(Category).filter_by(name = request.form['category']).one()
-        newItem = CategoryItem(name = request.form['name'],
-                              description= request.form['description'],
-                              category_id = category.id,
-                              user_id = login_session['user_id'])
+        category = session.query(Category)\
+            .filter_by(name=request.form['category']).one()
+        newItem = CategoryItem(name=request.form['name'],
+                               description=request.form['description'],
+                               category_id=category.id,
+                               user_id=login_session['user_id'])
         session.add(newItem)
         session.commit()
         return redirect(url_for('catalogs'))
     else:
-        return render_template('newCategoryItem.html', categories = categories, items = items)
+        return render_template('newCategoryItem.html',
+                               categories=categories, items=items)
 
-#Directs to edit an item page
+# Directs to edit an item page
 
-@app.route('/catalogs/<string:item_name>/edit/', methods = ['GET', 'POST'])
+
+@app.route('/catalogs/<string:item_name>/edit/', methods=['GET', 'POST'])
 def editItem(item_name):
+    # Directs to login page if not logged in
     if 'username' not in login_session:
         return redirect('/login/')
-    editedItem = session.query(CategoryItem).filter_by(name = item_name).first()
-    category = session.query(Category).filter_by(id = editedItem.category_id).one()
+    editedItem = session.query(CategoryItem)\
+        .filter_by(name=item_name).first()
+    category = session.query(Category)\
+        .filter_by(id=editedItem.category_id).one()
     categories = session.query(Category).all()
+    # Determines if user has access to edit item and sends error if not
     if login_session['user_id'] != editedItem.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to edit this item. Please create your own item at http://localhost:8000/catalogs/new in order to edit');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction()\
+            {alert('You are not authorized to edit this item.\
+            Please create your own item at http://localhost:8000/catalogs/new\
+            in order to edit');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
-        newCategory = session.query(Category).filter_by(name = request.form['category']).one()
+        newCategory = session.query(Category)\
+            .filter_by(name=request.form['category']).one()
         editedItem.category_id = newCategory.id
         editedItem.name = request.form['name']
         editedItem.description = request.form['description']
         session.add(editedItem)
         session.commit()
-        return redirect(url_for('catalogCategory', category_name = newCategory.name))
+        return redirect(url_for('catalogCategory',
+                                category_name=newCategory.name))
     else:
-        return render_template('editCategoryItem.html', item = editedItem, category = category, item_name = item_name, categories = categories)
+        return render_template('editCategoryItem.html',
+                               item=editedItem, category=category,
+                               item_name=item_name, categories=categories)
 
-    
-@app.route('/catalogs/<string:item_name>/delete/', methods = ['GET', 'POST'])
+
+@app.route('/catalogs/<string:item_name>/delete/', methods=['GET', 'POST'])
 def deleteItem(item_name):
+    # Directs to login page if not logged in
     if 'username' not in login_session:
         return redirect('/login/')
-    deletedItem = session.query(CategoryItem).filter_by(name = item_name).first()
-    category = session.query(Category).filter_by(id = deletedItem.category_id).one()
+    deletedItem = session.query(CategoryItem)\
+        .filter_by(name=item_name).first()
+    category = session.query(Category)\
+        .filter_by(id=deletedItem.category_id).one()
+    # Determines if user has access to delete item and sends error if not
     if login_session['user_id'] != deletedItem.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to delete this item. Please create your own item at http://localhost:8000/catalogs/new/ in order to delete it.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction()\
+        {alert('You are not authorized to delete this item.\
+        Please create your own item at http://localhost:8000/catalogs/new/\
+        in order to delete it.');}</script><body onload='myFunction()''>"
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
-        return redirect(url_for('catalogCategory', category_name = category.name))
+        return redirect(url_for('catalogCategory',
+                                category_name=category.name))
     else:
-        return render_template('deleteCategoryItem.html', item_name = item_name, item = deletedItem, category = category)
-
-
-
+        return render_template('deleteCategoryItem.html',
+                               item_name=item_name, item=deletedItem,
+                               category=category)
 
 
 @app.route('/catalogs/categories/JSON')
@@ -297,11 +340,13 @@ def categoriesJSON():
     categories = session.query(Category).order_by(asc(Category.name))
     return jsonify(categories=[c.serialize for c in categories])
 
+
 @app.route('/catalogs/items/JSON')
 def itemsJSON():
     items = session.query(CategoryItem).order_by(asc(CategoryItem.name))
     return jsonify(items=[i.serialize for i in items])
-    
+
+
 if __name__ == '__main__':
     app.secret_key = 'catalog_secret_key'
     app.debug = True
